cf1b2af81288efce885afc574d954601f7909b89
Nate Gallagher <nathang003@proton.me>
2023-03-08 12:02:53 -0600

    added custom control UmlObjectControl

---
+++ ToolboxUI/Controls/UmlObjectControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToolboxUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for UmlObjectControl.xaml
+    /// </summary>
+    public partial class UmlObjectControl : UserControl
+    {
+        public UmlObjectControl()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get; set;
+        }
+        public List<string> Values
+        {
+            get; set;
+        }
+    }
+}
